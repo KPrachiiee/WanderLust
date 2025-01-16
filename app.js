c1f02e636b95e7b438ad13326ca8a8529ab5fcd7
Prachi <kshirsagarprachi73@gmail.com>
2025-01-16 16:34:46 +0530

    Add project Files

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const ExpressError=require("./utils/ExpressError.js");
 const session=require("express-session");
+const MongoStore=require("connect-mongo");
 const flash = require("connect-flash");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
@@ -19,26 +20,11 @@ const listingRouter = require("./routes/listings.js");
 const reviewRouter = require("./routes/reviews.js");
 const userRouter = require("./routes/user.js");
 
+const dbUrl ='mongodb+srv://Prachi_k:Prachi123@cluster0.lca7z.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
 
-//const MONGO_URL = "mongodb://127.0.0.1:27017/airnub";
-const dbUrl ='mongodb+srv://Prachi_k:Prachi@123@cluster0.lca7z.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
-console.log(dbUrl);
-console.log(dbUrl);
-
-async function main()
-{
-    await mongoose.connect(dbUrl);
-}
-main()
-.then((res)=>{
-    console.log("Connected to DB");
-    console.log(res);
-})
-.catch(err=>{
-    console.log(err);
-});
-
-
+mongoose.connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to MongoDB successfully'))
+  .catch(err => console.error('MongoDB connection error:', err));
 
 // main()
 //   .then(() => {
@@ -60,8 +46,21 @@ app.use(methodOverride ("_method"));
 app.engine('ejs',ejsMate);
 app.use(express.static(path.join(__dirname,"/public")));
 
+const store = MongoStore.create({
+    mongoUrl:dbUrl,
+    crypto:{
+        secret:process.env.SECRET,
+    },
+    touchAfter: 24 * 3600,
+});
+
+store.on("error",()=>{
+    console.log("ERROR in MONGO SESSION STORE",err);
+});
+
 const sessionOptions={
-    secret:"mysupersecretcode",
+    store,
+    secret:process.env.SECRET,
     resave:false,
     saveUninitialized: true,
     cookie:{
@@ -74,6 +73,8 @@ const sessionOptions={
 // app.get("/",(req,res) => {
 //     res.send("Hi ,, I am root");
 // });
+
+
 
 //using flash 
 app.use(session(sessionOptions));
@@ -126,5 +127,3 @@ app.use((err,req,res,next)=>{
 app.listen(8080,() => {
     console.log("server is listening to port 8080");
 });
-
-
